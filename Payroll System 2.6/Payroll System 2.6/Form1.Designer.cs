c71b8866d4bf05acb668d53f3e8357261fc6dcd0
Code1Twilight <ronnelerrolsantos0@gmail.com>
2024-05-16 06:26:47 -0700

    Add files via upload

---
+++ Payroll System 2.6/Payroll System 2.6/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Payroll_System_2._6
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(63, 73);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(311, 20);
+            this.txt_username.TabIndex = 0;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(63, 164);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(311, 20);
+            this.txt_password.TabIndex = 1;
+            this.txt_password.UseSystemPasswordChar = true;
+            this.txt_password.TextChanged += new System.EventHandler(this.txt_password_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(64, 217);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(127, 56);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Log-in";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(247, 217);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(127, 56);
+            this.btn_close.TabIndex = 5;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(166, 284);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(113, 34);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 330);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Name = "frm_login";
+            this.Text = "Log-in";
+            this.Load += new System.EventHandler(this.frm_login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Payroll System 2.6/Payroll System 2.6/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Payroll_System_2._6
+{
+    public partial class frm_login : Form
+        
+    {
+        SqlConnection con = new SqlConnection("Data Source = DESKTOP - PI35E0Q\\SQLEXPRESS; Initial Catalog = db_payroll; Integrated Security = True");
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string query = "Select * From tbl_userpass Where Username = '" + txt_username.Text.Trim() + "' and Password = '" + txt_password.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+
+            if(dtbl.Rows.Count == 1)
+            {
+                timer1.Start();
+                progressBar1.Show();
+            }
+            else if (txt_username.Text == "" || txt_password.Text == "")
+            {
+                MessageBox.Show("Please fill up all fields");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username/password");
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frm_login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(35);
+            if(progressBar1.Value == progressBar1.Maximum)
+            {
+                timer1.Stop();
+                Mainform a = new Mainform();
+                this.Hide();
+                a.Show();
+            }
+        }
+    }
+}
